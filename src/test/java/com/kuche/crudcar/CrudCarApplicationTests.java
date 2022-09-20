7195bff54e70eb8a87cb63e8a4901deaf8a1ee1e
Steffen Kuche <kuche@subshell.com>
2022-09-20 21:16:53 +0200

    basic project setup

---
+++ src/test/java/com/kuche/crudcar/CrudCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuche.crudcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
